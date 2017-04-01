93bcee42990ddf2a4007832808dd9568731219f1
Emile Kroeger <flammifer@gmail.com>
2017-04-01 12:33:18 +0200

    start integrating icons and play with css masks

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -27,6 +27,10 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
+      .when('/icons', {
+        templateUrl: 'views/icons.html',
+        controller: 'IconsCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
---
+++ app/scripts/controllers/icons.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name iconicApp.controller:IconsCtrl
+ * @description
+ * # IconsCtrl
+ * Controller of the iconicApp
+ */
+angular.module('iconicApp')
+  .controller('IconsCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    var icons = ['ammonite', 'angular-spider', 'bat', 'bear-face', 'bee', 'bull-horns', 'butterfly', 'chess-knight', 'desert-skull', 'donkey', 'dragonfly', 'eagle-emblem', 'elephant', 'evil-bat', 'falcon-moon', 'feline', 'fox-head', 'frog', 'gecko', 'giant-squid', 'goat', 'gold-scarab', 'griffin-symbol', 'horse-head (1)', 'horse-head', 'horseshoe', 'hound', 'lion', 'lynx-head', 'minotaur', 'mounted-knight', 'octopus', 'ouroboros', 'owl', 'paw-print', 'pegasus', 'pig', 'poison', 'raven', 'salamander', 'scarab-beetle', 'scorpion', 'sea-serpent', 'seahorse', 'shark-jaws', 'snake-totem', 'snake', 'spider-alt', 'spiked-snail', 'squid-head', 'squid', 'stag-head', 'swan', 'turtle-shell', 'turtle', 'vulture', 'wasp-sting', 'werewolf', 'white-cat', 'wolf-head', 'wolf-howl'];
+    $scope.icons = [icons[0], icons[1]];
+  });
