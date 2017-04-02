30a66893a2cdb1b20816f7398f19c2e3560cac43
Emile Kroeger <flammifer@gmail.com>
2017-04-02 23:23:11 +0200

    make flag a bit more generic

--- app/scripts/controllers/flag.js
+++ app/scripts/controllers/flag.js
@@ -9,31 +9,42 @@
  */
 angular.module('iconicApp')
   .controller('FlagCtrl', function ($scope) {
-    var height = 200
-    var bannerstyle = {
-      //'-webkit-mask-box-image': iconUrl,
-      //'mask': iconUrl,
-      position: 'absolute',
-      left: "0px",
-      display: 'inline-block',
-      'background-color': "yellow",
-      height: (height - 40) + "px",
-      top: '60px',
-      width: "280px",
-      border: "20px solid red",
-      'border-right': 0,
-    };
-    var tailstyle = {
-      //'-webkit-mask-box-image': iconUrl,
-      position: 'absolute',
-      left: "300px",
-      top: '60px',
-      display: 'inline-block',
-      'border-left': "200px solid red",
-      'border-top': "100px solid rgba(0, 0, 0, 0)",
-      'border-bottom': "100px solid rgba(0, 0, 0, 0)",
-      height: "0px",
-    };
-    $scope.elements = [bannerstyle, tailstyle]
-    
+    $scope.elements = [];
+    function addTriangle(elements, x, y, wid, hei, color) {
+      elements.push({
+        left: x + "px",
+        top: y + "px",
+        'border-left': wid + "px solid " + color,
+        'border-top': (hei / 2) + "px solid rgba(0, 0, 0, 0)",
+        'border-bottom': (hei / 2) + "px solid rgba(0, 0, 0, 0)",
+        height: "0px",
+      });
+    }
+    function addRectangle(elements, x, y, wid, hei, color) {
+      elements.push({
+        left: x + "px",
+        top: y + "px",
+        height: hei + "px",
+        width: wid + "px",
+        "background-color": color,
+      });
+    }
+    function addHorizontalFlag(elements, x, y, wid, hei, widTail,
+      border, color, borderColor) {
+        // We need to calculate an offset to keep a correct diagonal
+        // border
+        var hypo = Math.sqrt(widTail * widTail + hei * hei);
+        var cosA = widTail / hypo;
+        var sinA = hei / hypo;
+        var shrink = border * (1 - cosA) / sinA;
+        addRectangle(elements, x, y, wid, hei, borderColor);
+        addRectangle(elements, x + border, y + border, wid - border - shrink,
+          hei - (2 * border), color);
+        addTriangle(elements, x + wid, y, widTail, hei, borderColor);
+        var subhei = hei - (2 * border);
+        var subwid = subhei * (widTail / hei)
+        addTriangle(elements, x + wid - shrink, y + border, subwid, subhei, color);
+    }
+    addHorizontalFlag($scope.elements, 0, 0, 300, 300, 70, 40,
+      "yellow", "red");
   });
