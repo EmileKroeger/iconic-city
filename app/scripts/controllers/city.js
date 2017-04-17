1b1c023481fcb6f80f76d221a6a176613b194bf7
Emile Kroeger <flammifer@gmail.com>
2017-04-17 16:09:59 +0200

    Fix classes in svg, and add support for different colors depending of orientation.

--- app/scripts/controllers/city.js
+++ app/scripts/controllers/city.js
@@ -38,8 +38,12 @@ angular.module('iconicApp')
       angular.forEach(this.tagColors, function(color, tag) {
         dynamicSvg.setTagColor(tag, color);
       });
-      angular.forEach(this.classColors, function(color, cls) {
-        dynamicSvg.setClassColor(cls, color);
+      angular.forEach(this.classColors, function(colors, cls) {
+        if (colors.constructor == Array) {
+          dynamicSvg.setClassColor(cls, colors[0], colors[1]);
+        } else {
+          dynamicSvg.setClassColor(cls, colors, colors);
+        }
       });
     };
     return ColorScheme;
@@ -185,10 +189,23 @@ angular.module('iconicApp')
         bottom: pos.y + 'px',
         left: pos.x + 'px',
       };
+      this.flip = false;
       if (pos.flip) {
         this.style.transform = 'scaleX(-1)';
+        this.flip = true;
       }
       //console.debug(this.style);
+    }
+    DynamicSvg.prototype.setElementColor = function(element, leftcolor, rightcolor) {
+      var isLeft = angular.element(element).hasClass("left");
+      if (this.flip) {
+        isLeft = !isLeft;
+      }
+      if (isLeft) {
+        element.setAttribute('fill', leftcolor);
+      } else {
+        element.setAttribute('fill', rightcolor);
+      }
     }
     DynamicSvg.prototype.setTagColor = function(tag, color) {
       // Helper: color one kind of tag
@@ -197,11 +214,11 @@ angular.module('iconicApp')
         elements[i].setAttribute('fill', color);
       }
     };
-    DynamicSvg.prototype.setClassColor = function(cls, color) {
+    DynamicSvg.prototype.setClassColor = function(cls, leftcolor, rightcolor) {
       // Helper: color one class
       var elements = this.svgDoc.getElementsByClassName(cls);
       for (var i=0; i<elements.length; i++) {
-        elements[i].setAttribute('fill', color);
+        this.setElementColor(elements[i], leftcolor, rightcolor);
       }
     };
     DynamicSvg.prototype.loaded = function() {
@@ -217,18 +234,22 @@ angular.module('iconicApp')
     //  'path': 'red',
     //});
     var blueWhite = new SColorScheme({
-      '.wall': 'white',
-      '.feature': 'blue',
-      '.roof': 'darkblue',
+      '.wall': ['lightgrey', 'white'],
+      '.feature': ['darkblue', 'blue'],
+      '.roof': ['darkblue', 'blue'],
     });
     var redWhite = new SColorScheme({
-      '.wall': 'white',
-      '.feature': 'red',
-      '.roof': 'red',
+      '.wall': ['lightgrey', 'white'],
+      '.feature': ['darkred', 'red'],
+      '.roof': ['darkred', 'red'],
     });
     var seriousColors = [blueWhite, redWhite];
-    var debugColors = [];
-    var colorBag = new SShuffleBag([blueWhite, redWhite], 3);
+    var debugColors = [new SColorScheme({
+      '.wall': ['darkred', 'red'],
+      '.feature': ['darkgreen', 'green'],
+      '.roof': ['darkblue', 'blue'],
+    })];
+    var colorBag = new SShuffleBag(seriousColors, 3);
     $scope.dynamicSvgs = [
       /*
       new SDynamicSvg('icons/chess-queen', red, {
