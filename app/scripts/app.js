56a97dc68ab2b95e5edf49c6c909af3d4f9851a8
Emile Kroeger <flammifer@gmail.com>
2017-04-29 01:43:27 +0200

    clean up and put cities on home page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -20,8 +20,8 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+        templateUrl: 'views/city.html',
+        controller: 'CityCtrl'
       })
       .when('/about', {
         templateUrl: 'views/about.html',
